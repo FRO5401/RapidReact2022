7a52d443fd81cbf6ddeecd361b370e4c5ff9a297
David Joseph Miller <56939567+ZombiKiller76er@users.noreply.github.com>
2022-02-04 15:54:28 -0500

    Fixed some things with Internal Mech, nothin crazy

--- RapidReact2022/src/main/java/frc/robot/Subsystems/InternalMech.java
+++ RapidReact2022/src/main/java/frc/robot/Subsystems/InternalMech.java
@@ -1,13 +1,37 @@
-package frc.robot.subsystems;
+package frc.robot.Subsystems;
 
-public class InternalMech {
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Robot;
+import frc.robot.Constants;
+import frc.robot.Controls;
+import edu.wpi.first.wpilibj.CounterBase.EncodingType;
+import edu.wpi.first.wpilibj.Solenoid;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+
+public class InternalMech extends SubsystemBase{
     private WPI_TalonSRX mech1;
     
-    public internalMech(){
-    mech1 = new WPI_TalonSRX(Constants.DriveConstants.DRIVE_MOTOR_LEFT_1);
-    mech1.setInverted(false);
-  
+    public InternalMech(){
+        mech1 = new WPI_TalonSRX(Constants.DriveConstants.DRIVE_MOTOR_LEFT_1);
+        mech1.setInverted(false);
 }
-
 
 }
