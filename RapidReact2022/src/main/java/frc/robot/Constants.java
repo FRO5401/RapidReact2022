4c2a97511d651da2f05d47692729ecadffb80c6e
IronKai123 <alirocks720@gmail.com>
2022-02-05 11:48:22 -0500

    compliant with new command system

--- RapidReact2022/src/main/java/frc/robot/Constants.java
+++ RapidReact2022/src/main/java/frc/robot/Constants.java
@@ -37,6 +37,26 @@ public class Constants {
     public static final double DRIVE_SENSITIVITY_DEFAULT = 1;
     public static final double SPIN_SENSITIVITY = 0.8;
 
+    // Buttons
+  public static final int XBOX_BUTTON_A = 1;
+  public static final int XBOX_BUTTON_B = 2;
+  public static final int XBOX_BUTTON_X = 3;
+  public static final int XBOX_BUTTON_Y = 4;
+  public static final int XBOX_BUTTON_LEFT_BUMPER = 5;
+  public static final int XBOX_BUTTON_RIGHT_BUMPER = 6;
+  public static final int XBOX_BUTTON_BACK = 7;
+  public static final int XBOX_BUTTON_START = 8;
+  public static final int XBOX_BUTTON_L3 = 9;
+  public static final int XBOX_BUTTON_R3 = 10;
+
+  // Axes
+  public static final int XBOX_AXIS_LEFT_X = 0;
+  public static final int XBOX_AXIS_LEFT_Y = 1;
+  public static final int XBOX_AXIS_LEFT_TRIGGER = 2;
+  public static final int XBOX_AXIS_RIGHT_TRIGGER = 3;
+  public static final int XBOX_AXIS_RIGHT_X = 4;
+  public static final int XBOX_AXIS_RIGHT_Y = 5;
+
     public static final int PCM_ID = 0;
 
     //Solenoids
