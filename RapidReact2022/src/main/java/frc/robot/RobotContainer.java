d7c4ad67a42083ae2d51bf19e64cdb1c3a0ede08
IronKai123 <alirocks720@gmail.com>
2022-02-22 17:57:07 -0500

    fixes from testing

--- RapidReact2022/src/main/java/frc/robot/RobotContainer.java
+++ RapidReact2022/src/main/java/frc/robot/RobotContainer.java
@@ -13,7 +13,6 @@ import edu.wpi.first.wpilibj2.command.RamseteCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
-import frc.robot.Autonomous.*;
 import frc.robot.Autonomous.groups.BallCenterTest;
 import frc.robot.Autonomous.groups.DoNothing;
 import frc.robot.Autonomous.groups.DriveStraight;
