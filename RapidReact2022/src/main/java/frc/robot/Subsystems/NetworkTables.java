1ae04976f8c86686eb52d15d34e83c501d123cc9
IronKai123 <alirocks720@gmail.com>
2022-02-23 22:17:19 -0500

    adding stuf

--- RapidReact2022/src/main/java/frc/robot/Subsystems/NetworkTables.java
+++ RapidReact2022/src/main/java/frc/robot/Subsystems/NetworkTables.java
@@ -8,24 +8,42 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class NetworkTables extends SubsystemBase {
   
-  NetworkTable ballTable;
+  NetworkTable ballTable, robotTable, targetTable;
   NetworkTableInstance inst;
   NetworkTableEntry ballXEntry, ballYEntry, ballDEntry, ballREntry;
+  NetworkTableEntry targetXEntry, targetYEntry, robotXEntry, robotYEntry;
+  NetworkTableEntry targetDEntry, robotDEntry;
   private static double ballX, ballY, ballDistance;
-  public double radius;
+  private static double targetX, targetY, robotX, robotY;
+  private static double targetDistance, robotDistance;
+  public double ballRadius;
 
 
   public NetworkTables() {
     inst = NetworkTableInstance.getDefault();
     ballTable = inst.getTable("Ball");
-    ballXEntry = ballTable.getEntry("cX");
-    ballYEntry = ballTable.getEntry("cY");
-    ballDEntry = ballTable.getEntry("distance");
-    ballREntry = ballTable.getEntry("radius");
+    targetTable = inst.getTable("Targets");
+    robotTable = inst.getTable("Robot");
+    ballXEntry = ballTable.getEntry("ballX");
+    ballYEntry = ballTable.getEntry("ballY");
+    targetXEntry = ballTable.getEntry("targetX");
+    targetYEntry = ballTable.getEntry("targetY");
+    robotXEntry = ballTable.getEntry("robotX");
+    robotYEntry = ballTable.getEntry("robotY");
+    ballDEntry = ballTable.getEntry("ballDistance");
+    targetDEntry = ballTable.getEntry("targetDistance");
+    robotDEntry = ballTable.getEntry("robotDistance");
+    ballREntry = ballTable.getEntry("ballRadius");
     ballX = 0.0;
     ballY = 0.0;
+    targetX = 0.0;
+    targetY = 0.0;
+    robotX = 0.0;
+    robotY = 0.0;
+    targetDistance = 0.0;
+    robotDistance = 0.0;
     ballDistance = 0.0;
-    radius = 0.0;
+    ballRadius = 0.0;
 
 
     inst.startClientTeam(5401); // where TEAM=190, 294, etc, or use inst.
@@ -44,8 +62,14 @@ public class NetworkTables extends SubsystemBase {
 
     ballX = ballXEntry.getDouble((ballX != 0) ? ballX : 0);
     ballY = ballYEntry.getDouble((ballY != 0) ? ballY : 0);
-    radius = (ballX == 0 && ballY == 0) ? 0 : ballREntry.getDouble((radius != 0) ? radius : 0);
+    ballRadius = (ballX == 0 && ballY == 0) ? 0 : ballREntry.getDouble((ballRadius != 0) ? ballRadius : 0);
     ballDistance = ballDEntry.getDouble((ballDistance != 0) ? ballDistance : 0);
+    targetY = targetYEntry.getDouble((targetY != 0) ? targetY : 0);
+    targetX = targetXEntry.getDouble((targetX != 0) ? targetX : 0);
+    robotX = robotXEntry.getDouble((robotX != 0) ? robotX : 0);
+    robotY = robotYEntry.getDouble((robotY != 0) ? robotY : 0);
+    robotDistance = robotDEntry.getDouble((robotDistance != 0) ? robotDistance : 0);
+    targetDistance = targetDEntry.getDouble((targetDistance != 0) ? targetDistance : 0);
     //powerPortX = powerPortXEntry.getDouble(0.0);
     //powerPortY = powerPortYEntry.getDouble(0.0);
     //System.out.println("The Ball coordinates are: " + "X: " + ballX + " Y: " + ballY);
@@ -54,12 +78,36 @@ public class NetworkTables extends SubsystemBase {
     //System.out.println("The Power Port coordinates are: " + "X: " + powerPortY + " Y: " + powerPortY);
   }
 
-  public double getBXValue() {
+  public double getBallBXValue() {
     return ballX;
   }
 
-  public double getBYValue() {
+  public double getBallBYValue() {
     return ballY;
+  }
+
+  public double getTargetXValue() {
+    return targetX;
+  }
+
+  public double getTargetYValue(){
+    return targetY;
+  }
+
+  public double getRobotXValue() {
+    return robotX;
+  }
+
+  public double getRobotYValue() {
+    return robotY;
+  }
+
+  public double getTargetDistance(){
+    return targetDistance;
+  }
+
+  public double getRobotDistance() {
+    return robotDistance;
   }
 
   public double getBallDistance(){
@@ -67,23 +115,30 @@ public class NetworkTables extends SubsystemBase {
   }
 
   public double getBallRadius(){
-    return radius;
+    return ballRadius;
   }
+
 
   public void resetValues(){
     ballX = 0;
     ballY = 0;
+    targetY = 0;
+    targetX = 0;
+    robotX = 0;
+    robotY = 0;
+    robotDistance = 0;
+    targetDistance = 0;
     ballDistance = 0;
-    radius = 0;
+    ballRadius = 0;
   }
 
   public boolean checkCentered(){
 
-    if(getBXValue() >= 320 && getBXValue() <= 420){
+    if(getBallBXValue() >= 320 && getBallBXValue() <= 420){
       return true;
     }
     
-    else if(((getBXValue() < 320) & (getBXValue() > 0)) || ((getBXValue() > 420) & (getBXValue() < 800))){
+    else if(((getBallBXValue() < 320) & (getBallBXValue() > 0)) || ((getBallBXValue() > 420) & (getBallBXValue() < 800))){
       return false;
     }
     return false;
@@ -93,8 +148,8 @@ public class NetworkTables extends SubsystemBase {
 
   public void reportValues()
   {
-    SmartDashboard.putNumber("Current Ball X", getBXValue());
-    SmartDashboard.putNumber("Current Ball Y", getBYValue());
+    SmartDashboard.putNumber("Current Ball X", getBallBXValue());
+    SmartDashboard.putNumber("Current Ball Y", getBallBYValue());
     SmartDashboard.putNumber("Current Ball radius", getBallRadius());
     SmartDashboard.putNumber("Ball Distance", getBallDistance());
   }
